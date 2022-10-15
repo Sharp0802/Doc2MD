d6debafc0e9985f224dc945f723df09689d6d086
Sharp0802 <sharp0802.business@gmail.com>
2022-10-15 14:24:21 +0900

    Start to impl markdown renderer: Work on progress...

---
+++ src/Doc2MD/Doc2MD/Renderer.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Xml;
+
+namespace Doc2MD;
+
+internal class Renderer
+{
+    
+    private string T(string t, object[] p) => string.Format(t, p);
+    
+    public string ApplyTemplate(string key, string value)
+    {
+        return key switch
+        {
+            "summary" => $"{value}",
+            "remarks" => $"## Remarks\n\n{value}",
+            "returns" => $"### Returns\n\n{value}",
+            "param" => $"### Parameters\n\n{value}",
+            "typeparam" => $"### Type Parameters\n\n{value}",
+            "exception" => $"### Exceptions\n\n{value}",
+            "value" => $"### Property Value\n\n{value}",
+            
+            "seealso" => $"## See Also\n\n{value}"
+        };
+    }
+    
+    public string RenderMember(XmlElement element)
+    {
+        var sections = new Dictionary<string, string>();
+        foreach (var section in element.ChildNodes
+                     .OfType<XmlElement>()
+                     .GroupBy(e => e.Name))
+        {
+            var builder = new StringBuilder();
+            foreach (var item in section) 
+                builder.Append(Render(item)).Append(" \n");
+
+            sections.Add(section.Key, builder.ToString());
+        }
+    }
+    
+    public string Render(XmlNode node)
+    {
+        switch (node)
+        {
+            case XmlElement element:
+                Render(element);
+                break;
+            case XmlText text:
+                Render(text);
+                break;
+        }
+    }
+
+    public string Render(XmlElement element)
+    {
+        switch (element.Name)
+        {
+            
+        }
+    }
+
+    public string Render(XmlText text) => text.Value;
+}
